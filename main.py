6aa701ed6e399b6fe32c18a5e301d78f19856671
Nick Kingueleoua <nick.kingueloua@airudi.com>
2025-06-17 14:02:40 +0000

    Some sample code

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ print((15, "Nick"))
 
 for i in range(10):
     print(f"--- ------  {i}")
+
+
+class Person:
+    God = "YHWH"
+
+    def __init__(self, name, age):
+        self.father: Person | None = None
+        self.__name = name
+        self.age = age
+
+    def __str(self):
+        return "Mbi la so"
+
+p1 = Person("John", 36)
+
+print(p1.__name)
+print(Person.God)
